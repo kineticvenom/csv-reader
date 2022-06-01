666d76f7513c1fd097837b1f976d300905ec7327
kineticvenom <kalebvarnes@yahoo.com>
2022-05-31 20:49:13 -1000

    No Spaces! Solved!

--- main.py
+++ main.py
@@ -6,37 +6,36 @@ def available_for_adoption(user_input) :
         with open ('data/cats.csv', 'r' ) as cats_file:
             rows = csv.reader(cats_file, delimiter=',')
             title = next(rows)
+            
             data = {}
             cats_list = []
             
-            
             for row in rows:     
-                data[title[0]] = row[0]
-                data[title[1]] = row[1]
-                data[title[2]] = row[2]
-                cats_list.append(f"{data['name']} is a {data[' age']} year old {data[' breed']}")
-
-            for cat in cats_list :        
-                cat = cat.replace("  ", " ")
-                cats_list.append(cat) 
+               
+                data[title[0].replace(" ","")] = row[0]
+                data[title[1].replace(" ","")] = row[1].replace(" ","")
+                data[title[2].replace(" ","")] = row[2].replace(" ","")
+                cats_list.append(f"{data['name']} is a {data['age']} year old {data['breed']}")
         return cats_list
     elif user_input == "dogs" :
        with open ('data/dogs.csv', 'r' ) as dogs_file:
             rows = csv.reader(dogs_file, delimiter=',')
             title = next(rows)
             data = {}
-            
-            
-            for row in rows:  
-                
-                data[title[0]] = row[0]
-                data[title[1]] = row[1]
-                data[title[2]] = row[2]
-                print (f"{data['name']} is a {data[' age']} year old {data[' breed']}")
+            dogs_list = []
+                    
+            for row in rows:                  
+                data[title[0].replace(" ","")] = row[0]
+                data[title[1].replace(" ","")] = row[1].replace(" ","")
+                data[title[2].replace(" ","")] = row[2].replace(" ","")
+                dogs_list.append(f"{data['name']} is a {data['age']} year old {data['breed']}")
+            return dogs_list
     else :
         return (f"Sorry we do not have any {user_input}")     
 
+print(available_for_adoption("dogs"))
 print(available_for_adoption("cats"))
+print(available_for_adoption("snakes"))
 
 # print(data)
 # for item in data:
